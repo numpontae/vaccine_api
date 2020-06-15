05083718eefebfcb9f22ed33a5a9b0cd690f4603
Peerapong Kraisrisuwan <p.kraisrisuwan@gmail.com>
2020-06-15 07:59:57 +0700

    add auto address

--- src/routes/ct.routes.ts
+++ src/routes/ct.routes.ts
@@ -63,6 +63,71 @@ class ctRoute {
       res.send(result) 
     }
   }
+  getZip() {
+    return async (req: Request, res: Response) => {
+      let { zip, type } = req.query
+      let repos = di.get('repos')
+      let query = ''
+      if ( type == '1' ) {
+        query = `SELECT * FROM CT_Zip Where Zip_Code NOT IN ('JAN-64', 'AUG-43', '999999', '') AND Zip_Code = '${zip}'`
+      } else {
+        query = `SELECT * FROM CT_Zip Where Zip_Code NOT IN ('JAN-64', 'AUG-43', '999999', '') AND Zip_Code LIKE '%${zip}%'`
+      }
+      let result = await repos.query(query)
+      res.send(result) 
+    }
+  }
+  getProvince() {
+    return async (req: Request, res: Response) => {
+      let { id } = req.query
+      let repos = di.get('repos')
+      let query = ''
+      query = `SELECT * FROM CT_Province WHERE Code NOT IN ('999', '900') AND ID = '${id}'`
+      
+      let result = await repos.query(query)
+      res.send(result) 
+    }
+  }
+  getCityArea() {
+    return async (req: Request, res: Response) => {
+      let { zip } = req.query
+      let repos = di.get('repos')
+      let query = ''
+      query = `SELECT ca.* FROM CT_Zip z
+              RIGHT JOIN CT_CityArea ca ON ca.ID = z.Cityarea_ID
+              WHERE ca.Code NOT IN ('999999', '900000') AND z.Zip_Code = '${zip}'
+              GROUP BY ca.ID`
+      
+      let result = await repos.query(query)
+      res.send(result) 
+    }
+  }
+  getCity() {
+    return async (req: Request, res: Response) => {
+      let { id } = req.query
+      let repos = di.get('repos')
+      let query = ''
+      query = `SELECT * FROM CT_City WHERE ID = '${id}'`
+      
+      let result = await repos.query(query)
+      res.send(result) 
+    }
+  }
+  getTest() {
+    return async (req: Request, res: Response) => {
+      let repos = di.get('repos')
+      let query = `SELECT * FROM Patient_Social;`
+      let result = await repos.query(query)
+      let data = result.map((d: any) => {
+        let data = {
+          id: d.PatientID,
+          data: JSON.parse(d.Detail)
+        }
+        return data
+      })
+      res.send(data) 
+    }
+  }
 }
 
 const router = Router()
@@ -75,5 +140,10 @@ router
   .get("/religion", route.getReligion())
   .get("/preferredlanguage", route.getPreferredLanguage())
   .get("/country", route.getCountry())
+  .get("/test", route.getTest())
+  .get("/zip", route.getZip())
+  .get("/province", route.getProvince())
+  .get("/cityarea", route.getCityArea())
+  .get("/city", route.getCity())
 
 export const ct = router
