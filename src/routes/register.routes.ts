131cc24d70e0ea6dcdcb74ee5757598d03ed35e6
Peerapong Kraisrisuwan <p.kraisrisuwan@gmail.com>
2020-06-17 03:44:19 +0700

    add search child and fix regis

--- src/routes/register.routes.ts
+++ src/routes/register.routes.ts
@@ -24,7 +24,7 @@ class registerRoute {
         '${body.patient_info.address ? body.patient_info.address : ''}', '${body.patient_info.country}', '${body.patient_info.phone_no ? body.patient_info.phone_no : ''}', 
         '${body.patient_info.email ? body.patient_info.email : ''}', '${body.patient_info.homephone ? body.patient_info.homephone : ''}', '${body.patient_info.officephone ? body.patient_info.officephone : ''}', 0, ${body.consent}, ${body.type})`;
         let Info = await repos.query(insertInfo);
-        let insertPermanent = `INSERT INTO Permanent_Address (Patient_ID, Country, Postcode, Subdistrict, District, Address, Province) VALUES(${Info.insertId}, ${body.permanent.country}, '${body.permanent.postcode}', ${body.permanent.subdistrict}, '${body.permanent.district}', '${body.permanent.address}', '${body.permanent.province}');`
+        let insertPermanent = `INSERT INTO Permanent_Address (Patient_ID, Country, Postcode, Subdistrict, District, Address, Province) VALUES(${Info.insertId}, ${body.permanent.country}, '${body.permanent.postcode ? body.permanent.postcode : ''}', ${body.permanent.subdistrict}, '${body.permanent.district ? body.permanent.district : ''}', '${body.permanent.address ? body.permanent.address : ''}', '${body.permanent.province ? body.permanent.province : ''}');`
         await repos.query(insertPermanent);
         let presentAddress = {
           Country: body.present.sameAddress ? body.permanent.country : body.present.country,
@@ -34,7 +34,7 @@ class registerRoute {
           Address: body.present.sameAddress ? body.permanent.address : body.present.address,
           Province: body.present.sameAddress ? body.permanent.province : body.present.province
         }
-        let insertPresent = `INSERT INTO Present_Address (Patient_ID, Country, Postcode, Subdistrict, District, Address, Province, sameAddress) VALUES(${Info.insertId}, ${presentAddress.Country}, '${presentAddress.Postcode}', ${presentAddress.Subdistrict}, '${presentAddress.District}', '${presentAddress.Address}', '${presentAddress.Province}', ${body.present.sameAddress});`
+        let insertPresent = `INSERT INTO Present_Address (Patient_ID, Country, Postcode, Subdistrict, District, Address, Province, sameAddress) VALUES(${Info.insertId}, ${presentAddress.Country}, '${presentAddress.Postcode ? presentAddress.Postcode : ''}', ${presentAddress.Subdistrict}, '${presentAddress.District ? presentAddress.District : ''}', '${presentAddress.Address ? presentAddress.Address : ''}', '${presentAddress.Province ? presentAddress.Province : ''}', ${body.present.sameAddress});`
         await repos.query(insertPresent);
         let emergencyAddress = {
           Country: body.emergency.sameAddress ? body.permanent.country : body.emergency.country,
@@ -44,7 +44,7 @@ class registerRoute {
           Address: body.emergency.sameAddress ? body.permanent.address : body.emergency.addressaddress,
           Province: body.emergency.sameAddress ? body.permanent.province : body.emergency.province
         }
-        let insertEmergency = `INSERT INTO Emergency (Patient_ID, Firstname, Lastname, Relation, Country, Postcode, Province, Subdistrict, District, Address, sameAddress, Email, Phone_No) VALUES(${Info.insertId}, '${body.emergency.first_name}', '${body.emergency.last_name}', '${body.emergency.relation}', ${emergencyAddress.Country}, '${emergencyAddress.Postcode}', '${emergencyAddress.Province}', '${emergencyAddress.Subdistrict}', '${emergencyAddress.District}', '${emergencyAddress.Address}', ${body.emergency.sameAddress}, '${body.emergency.email}', '${body.emergency.phone_no}');`
+        let insertEmergency = `INSERT INTO Emergency (Patient_ID, Firstname, Lastname, Relation, Country, Postcode, Province, Subdistrict, District, Address, sameAddress, Email, Phone_No) VALUES(${Info.insertId}, '${body.emergency.first_name}', '${body.emergency.last_name}', '${body.emergency.relation}', ${emergencyAddress.Country}, '${emergencyAddress.Postcode ? emergencyAddress.Postcode : ''}', '${emergencyAddress.Province ? emergencyAddress.Province : ''}', '${emergencyAddress.Subdistrict}', '${emergencyAddress.District ? emergencyAddress.District : ''}', '${emergencyAddress.Address ? emergencyAddress.Address : ''}', ${body.emergency.sameAddress}, '${body.emergency.email}', '${body.emergency.phone_no}');`
         await repos.query(insertEmergency);
         let insertFinan = `INSERT INTO Financial (Self_Pay, Company_Contact, Insurance, Company, Payment_As, Patient_ID) VALUES(${_.indexOf(body.financial.payment_method, 'Self pay') >= 0 ? 0 : 1}, ${_.indexOf(body.financial.payment_method, 'Company contact') >= 0 ? 0 : 1}, ${_.indexOf(body.financial.payment_method, 'Insurance') >= 0 ? 0 : 1}, '${body.financial.company ? body.financial.company : ''}', '${body.financial.payment_as}', ${Info.insertId});`
         await repos.query(insertFinan);
@@ -77,7 +77,6 @@ class registerRoute {
           await repos.query(insertExercise)
         }
         if (body.personal_history.alcohol.status == 'Yes') {
-          console.log(JSON.stringify(body.personal_history.alcohol.detail))
           let insertAlcohol = `INSERT INTO Patient_Social (PatientID, Habit, Status, Quantity, Detail, Comment) VALUES(${Info.insertId}, 'alcohol', '${body.personal_history.alcohol.status}', '${body.personal_history.alcohol.quantity}', '${JSON.stringify(body.personal_history.alcohol.detail)}', '${body.personal_history.alcohol.comment ? body.personal_history.alcohol.comment : ''}');`
           await repos.query(insertAlcohol)
         }
@@ -93,14 +92,42 @@ class registerRoute {
         res.send({message: 'Success'})
       } else if ( body.type == 1 ) {
         let insertInfo = `INSERT INTO Patient_Info (Title, Firstname, Middlename, Lastname, 
-        DOB, Gender, Nationality, Address, Country, Phone_No, 
+        DOB, Gender, Nationality, Phone_No, 
         Email, Confirm, Consent, Type) 
-        VALUES('${body.patient_info.title ? body.patient_info.title : ''}', '${body.patient_info.firstname ? body.patient_info.firstname : ''}', 
-        '${body.patient_info.middlename ? body.patient_info.middlename : ''}', '${body.patient_info.lastname ? body.patient_info.lastname : ''}', 
-        '${body.patient_info.dob}', ${body.patient_info.gender}, ${body.patient_info.nationality}, 
-        '${body.patient_info.address ? body.patient_info.address : ''}', '${body.patient_info.country}', '${body.patient_info.phone_no ? body.patient_info.phone_no : ''}', 
-        '${body.patient_info.email ? body.patient_info.email : ''}', 0, ${body.consent}, ${body.type})`;
+        VALUES('${body.general_info.title ? body.general_info.title : ''}', '${body.general_info.firstname ? body.general_info.firstname : ''}', 
+        '${body.general_info.middlename ? body.general_info.middlename : ''}', '${body.general_info.lastname ? body.general_info.lastname : ''}', 
+        '${body.general_info.dob}', ${body.general_info.gender}, ${body.general_info.nationality}, '${body.general_info.phone_no ? body.general_info.phone_no : ''}', 
+        '${body.general_info.email ? body.general_info.email : ''}', 0, ${body.consent}, ${body.type})`;
         let Info = await repos.query(insertInfo);
+        let insertPermanent = `INSERT INTO Permanent_Address (Patient_ID, Country, Postcode, Subdistrict, District, Address, Province) VALUES(${Info.insertId}, ${body.permanent.country}, '${body.permanent.postcode ? body.permanent.postcode : ''}', ${body.permanent.subdistrict}, '${body.permanent.district ? body.permanent.district : ''}', '${body.permanent.address ? body.permanent.address : ''}', '${body.permanent.province ? body.permanent.province : ''}');`
+        let presentAddress = {
+          Country: body.present.sameAddress ? body.permanent.country : body.present.country,
+          Postcode: body.present.sameAddress ? body.permanent.postcode : body.present.postcode,
+          Subdistrict: body.present.sameAddress ? body.permanent.subdistrict : body.present.subdistrict,
+          District: body.present.sameAddress ? body.permanent.district : body.present.district,
+          Address: body.present.sameAddress ? body.permanent.address : body.present.address,
+          Province: body.present.sameAddress ? body.permanent.province : body.present.province
+        }
+        let insertPresent = `INSERT INTO Present_Address (Patient_ID, Country, Postcode, Subdistrict, District, Address, Province, sameAddress) VALUES(${Info.insertId}, ${presentAddress.Country}, '${presentAddress.Postcode ? presentAddress.Postcode : ''}', ${presentAddress.Subdistrict}, '${presentAddress.District ? presentAddress.District : ''}', '${presentAddress.Address ? presentAddress.Address : ''}', '${presentAddress.Province ? presentAddress.Province : ''}', ${body.present.sameAddress});`
+        let insertGuardian = `INSERT INTO Guardian (Title, Firstname, Lastname, Relation, Patient_ID) VALUES('${body.patient_info.title}', '${body.patient_info.firstname}', '${body.patient_info.lastname}', '${body.patient_info.relation}', ${Info.insertId});`
+        let insertFinan = `INSERT INTO Financial (Self_Pay, Company_Contact, Insurance, Patient_ID) VALUES(${_.indexOf(body.patient_info.payment_method, 'Self pay') >= 0 ? 0 : 1}, ${_.indexOf(body.patient_info.payment_method, 'Company contact') >= 0 ? 0 : 1}, ${_.indexOf(body.patient_info.payment_method, 'Insurance') >= 0 ? 0 : 1}, ${Info.insertId});`
+        let insertPediatric = `INSERT INTO Pediatric (Pob, Blood_Group, Weight, Length, Head, Delivery, Tsh, Psu, Hearing, Pab, Didm, Drug, Food, Other, Psci, Curmed, Allergies, Patient_ID) VALUES('${body.pediatric.pob}', '${body.pediatric.blood_group}', ${body.pediatric.weight}, ${body.pediatric.length}, ${body.pediatric.head}, '${body.pediatric.delivery}', '${body.pediatric.tsh}', '${body.pediatric.psu}', '${body.pediatric.hearing}', '${body.pediatric.pab}', '${body.pediatric.didm}', '${body.pediatric.drug}', '${body.pediatric.food}', '${body.pediatric.other}', '${body.pediatric.psci}', '${body.pediatric.curmed}', '${body.pediatric.allergies}', ${Info.insertId});`
+        await repos.query(insertPermanent);
+        await repos.query(insertPresent);
+        await repos.query(insertGuardian);
+        await repos.query(insertFinan);
+        await repos.query(insertPediatric);
+        if (body.siblings.family.length > 0) {
+          let valuesFamily: any[] = [] 
+          body.siblings.family.map((p: any) => {
+            let value = [Info.insertId, `${p}`]
+            valuesFamily.push(value) 
+          })
+          let insertFamily = `INSERT INTO Family_History (Patient_ID, Disease) VALUES ?;`
+          await repos.query(insertFamily, [valuesFamily])
+        }
+        
+        res.send({message: 'Success'})
       }
     };
   }
@@ -132,67 +159,104 @@ class registerRoute {
         res.send(data)
       } else {
         //let query = `SELECT PI.*, CTS.Desc_EN Gender_Desc, CTN.Desc_EN Nation_Desc, CTR.Desc_EN Religion_Desc, CTP.Desc_EN Lan_Desc FROM Patient_Info PI`
-        let query = `SELECT PI.Title title, PI.Firstname firstname, PI.Middlename middlename, PI.Lastname lastname, PI.DOB dob, PI.Gender gender, PI.Nationality nationality, PI.Religion religion, PI.Preferred_Language preferredlanguage, PI.Expatriate expatriate, PI.Marital_Status marital_status, PI.National_ID national_id, PI.Passport passport, PI.Occupation occupation, PI.Address address, PI.Country country, PI.Phone_No phone_no, PI.Email email, PI.Homephone homephone, PI.Officephone officephone, PI.Consent consent FROM Patient_Info PI`
+        let query = `SELECT PI.Title title, PI.Firstname firstname, PI.Middlename middlename, PI.Lastname lastname, PI.DOB dob, PI.Gender gender, PI.Nationality nationality, PI.Religion religion, PI.Preferred_Language preferredlanguage, PI.Expatriate expatriate, PI.Marital_Status marital_status, PI.National_ID national_id, PI.Passport passport, PI.Occupation occupation, PI.Address address, PI.Country country, PI.Phone_No phone_no, PI.Email email, PI.Homephone homephone, PI.Officephone officephone, PI.Consent consent, PI.Type type FROM Patient_Info PI`
         query += ` WHERE 1 = 1`
         query += ` AND PI.Id = ${id}`
-        let permanentQuery = `SELECT * FROM Permanent_Address WHERE Patient_ID = ${id}`
-        let presentQuery = `SELECT * FROM Present_Address WHERE Patient_ID = ${id}`
-        let emerQuery = `SELECT * FROM Emergency WHERE Patient_ID = ${id}`
-        let finanQuery = `SELECT * FROM Financial WHERE Patient_ID = ${id}`
-        let hisQuery = `SELECT * FROM Patient_History WHERE Patient_ID = ${id}`
-        let diseasesQuery = `SELECT * FROM Patient_Diseases WHERE Patient_ID = ${id}`
-        let familyQuery = `SELECT * FROM Family_History WHERE Patient_ID = ${id}`
-        let socialQuery = `SELECT * FROM Patient_Social WHERE PatientID = ${id}`
         let info = await repos.query(query)
-        let permanent = await repos.query(permanentQuery)
-        let present = await repos.query(presentQuery)
-        let emer = await repos.query(emerQuery)
-        let finan = await repos.query(finanQuery)
-        let his = await repos.query(hisQuery)
-        let diseases = await repos.query(diseasesQuery)
-        let family = await repos.query(familyQuery)
-        let social = await repos.query(socialQuery)
-        let dataSocial = social.map((d: any) => {
-          let data = {
-            Patient_ID: d.PatientID,
-            Habit: d.Habit,
-            Status: d.Status,
-            Quantity: d.Quantity,
-            Detail: JSON.parse(d.Detail),
-            Comment: d.Comment
+        if (info[0].type == 0) {
+          let permanentQuery = `SELECT * FROM Permanent_Address WHERE Patient_ID = ${id}`
+          let presentQuery = `SELECT * FROM Present_Address WHERE Patient_ID = ${id}`
+          let emerQuery = `SELECT * FROM Emergency WHERE Patient_ID = ${id}`
+          let finanQuery = `SELECT * FROM Financial WHERE Patient_ID = ${id}`
+          let hisQuery = `SELECT * FROM Patient_History WHERE Patient_ID = ${id}`
+          let diseasesQuery = `SELECT * FROM Patient_Diseases WHERE Patient_ID = ${id}`
+          let familyQuery = `SELECT * FROM Family_History WHERE Patient_ID = ${id}`
+          let socialQuery = `SELECT * FROM Patient_Social WHERE PatientID = ${id}`
+          let permanent = await repos.query(permanentQuery)
+          let present = await repos.query(presentQuery)
+          let emer = await repos.query(emerQuery)
+          let finan = await repos.query(finanQuery)
+          let his = await repos.query(hisQuery)
+          let diseases = await repos.query(diseasesQuery)
+          let family = await repos.query(familyQuery)
+          let social = await repos.query(socialQuery)
+          let dataSocial = social.map((d: any) => {
+            let data = {
+              Patient_ID: d.PatientID,
+              Habit: d.Habit,
+              Status: d.Status,
+              Quantity: d.Quantity,
+              Detail: JSON.parse(d.Detail),
+              Comment: d.Comment
+            }
+            return data
+          })
+          let payment = []
+          let diseaseslist: any = []
+          let familylist: any = []
+          if (finan.length) {
+            if (finan[0].Self_Pay == 0) payment.push('Self pay')
+            if (finan[0].Company_Contact == 0) payment.push('Company contact')
+            if (finan[0].Insurance == 0) payment.push('Insurance')
           }
-          return data
-        })
-        let payment = []
-        let diseaseslist: any = []
-        let familylist: any = []
-        if (finan.length) {
-          if (finan[0].Self_Pay == 0) payment.push('Self pay')
-          if (finan[0].Company_Contact == 0) payment.push('Company contact')
-          if (finan[0].Insurance == 0) payment.push('Insurance')
+          diseases.map((d:any) => {
+            diseaseslist.push(d.Disease)
+          })
+          family.map((d:any) => {
+            familylist.push(d.Disease)
+          })
+          let result = {
+            info: info[0],
+            permanent: permanent[0],
+            present: present[0],
+            emer: emer[0],
+            finan: {
+              payment:payment,
+              company: finan[0].Company,
+              payment_as: finan[0].Payment_As
+            },
+            his: his[0],
+            diseases: diseaseslist,
+            family: familylist,
+            social: dataSocial
+          }
+          res.send(result)
+        } else {
+          let permanentQuery = `SELECT * FROM Permanent_Address WHERE Patient_ID = ${id}`
+          let presentQuery = `SELECT * FROM Present_Address WHERE Patient_ID = ${id}`
+          let financialQuery = `SELECT * FROM Financial WHERE Patient_ID = ${id}`
+          let familyQuery = `SELECT * FROM Family_History WHERE Patient_ID = ${id}`
+          let guardianQuery = `SELECT * FROM Guardian WHERE Patient_ID = ${id}`
+          let pediatricQuery = `SELECT * FROM Pediatric WHERE Patient_ID = ${id}`
+          let permanent = await repos.query(permanentQuery)
+          let present = await repos.query(presentQuery)
+          let family = await repos.query(familyQuery)
+          let financial = await repos.query(financialQuery)
+          let guardian = await repos.query(guardianQuery)
+          let pediatric = await repos.query(pediatricQuery)
+          let payment = []
+          let familylist: any = []
+          if (financial.length) {
+            if (financial[0].Self_Pay == 0) payment.push('Self pay')
+            if (financial[0].Company_Contact == 0) payment.push('Company contact')
+            if (financial[0].Insurance == 0) payment.push('Insurance')
+          }
+          family.map((d:any) => {
+            familylist.push(d.Disease)
+          })
+          let result = {
+            info: info[0],
+            guardian: guardian[0],
+            finan: {
+              payment: payment
+            },
+            permanent: permanent[0],
+            present: present[0],
+            pediatric: pediatric[0],
+            family: familylist,
+          }
+          res.send(result)
         }
-        diseases.map((d:any) => {
-          diseaseslist.push(d.Disease)
-        })
-        family.map((d:any) => {
-          familylist.push(d.Disease)
-        })
-        let result = {
-          info: info[0],
-          permanent: permanent[0],
-          present: present[0],
-          emer: emer[0],
-          finan: {
-            payment:payment,
-            company: finan[0].Company,
-            payment_as: finan[0].Payment_As
-          },
-          his: his[0],
-          diseases: diseaseslist,
-          family: familylist,
-          social: dataSocial
-        }
-        res.send(result)
       }
       
     }
@@ -200,7 +264,6 @@ class registerRoute {
   saveSignature() {
     return async (req: Request, res: Response) => {
       let { signatureHash, signatureImage, id, consent } = req.body;
-      console.log(req.body)
       let repos = di.get("repos");
       let query = `UPDATE Patient_Info SET Confirm=1, Consent=${consent} WHERE Id=${id};`
       let insertSignature = `INSERT INTO Signature (Patient_ID, Hash_Data, Image) VALUES(${id}, '${signatureHash}', '${signatureImage}');`
