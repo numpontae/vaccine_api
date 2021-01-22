f9f5e51980d9dfb5fcf8b25cf5673b2252238bfc
numpontae <numpontae09@gmail.com>
2021-01-22 08:41:57 +0700

    commit

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,5 @@
 import { ct } from "./ct.routes";
+import { registration } from "./registration.routes";
 export class Routes {
   constructor(private app: any) {
     this.app = app;
@@ -7,5 +8,6 @@ export class Routes {
   setRoutes() {
     const prefix = "/api/v1";
     this.app.use(prefix + "/ct", ct);
+    this.app.use(prefix + "/registration", registration);
   }
 }
